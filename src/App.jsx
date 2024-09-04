b87a74594ebe2ba1bd7fe4a669a12e07228f1242
Vishal <vishal.bhumca21@gmail.com>
2024-09-04 09:40:26 +0530

    centered product items

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import ProductCatalog from "./component/ProductCatalog";
 function App() {
   return (
     <>
-        <section className="relative px-4 bg-neutral-100">
+        <section className="relative w-full  px-4 bg-neutral-100">
             <Navbar />
             <header className="w-full flex justify-center my-2">
                 <h1 className="font-bold text-[1.5em] text-gray-500 border-b border-gray-800 w-fit">
                     New Products
                 </h1>
             </header>
-            <main>
+            <main className="w-full grid justify-center">
               <ProductCatalog/>
             </main>
         </section>
