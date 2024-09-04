06230f44c58f8d07e9c198e4863e7e7f5c6b4fec
Vishal <vishal.bhumca21@gmail.com>
2024-09-04 10:16:20 +0530

    sticky navbar and cart

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import ProductCatalog from "./component/ProductCatalog";
 function App() {
   return (
     <>
+        <Navbar />
         <section className="relative w-full  px-4 bg-neutral-100">
-            <Navbar />
             <header className="w-full flex justify-center my-2">
                 <h1 className="font-bold text-[1.5em] text-gray-500 border-b border-gray-800 w-fit">
                     New Products
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import Cart from "./Cart";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,34 +6,31 @@ import { addItemToCart } from "../features/Cart/CartSlice";
 
 const Navbar = () => {
     const [isCartOpen, setCartOpen] = useState(false);
-    const [isCartAnimation, setCartAnimation] = useState(false);
     const cartItems = useSelector((state) => state.cart.cartItems);
-    const dispatch = useDispatch();
 
     const openCart = () => setCartOpen(true);
     const closeCart = () => setCartOpen(false);
 
     const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 
-
     return (
         <>
-            <nav className="w-full border-b border-gray-700 mb-4 flex items-center justify-between py-3">
+            <nav className="sticky top-0 z-30 bg-white bg-opacity-65 backdrop-blur-md w-full border-b border-gray-700 flex items-center justify-between py-3 px-6">
                 <h1 className="text-[1.8em] font-bold font-[Libre Baskerville] italic">
                     Digi <span className="text-green-500 font-extrabold">Market</span>
                 </h1>
                 <section className="flex items-center justify-center gap-x-8 hover:text-red-600 hover:font-bold transition-all">
                     <h1
-                        className={`relative flex justify-center items-center gap-x-3 px-4 cursor-pointer ${isCartAnimation ? 'animate-cart' : ''}`}
+                        className="relative flex justify-center items-center gap-x-3 px-4 cursor-pointer"
                         onClick={openCart}
                     >
-                        <FaShoppingCart className="relative " />
+                        <FaShoppingCart className="relative" />
                         {totalItems > 0 && (
                             <span className="absolute top-[2px] right-[-10px] text-white text-[0.8em] bg-gray-800 font-bold rounded-full w-6 h-6 flex items-center justify-center p-2">
                                 {totalItems}
                             </span>
                         )}
-                        <span className="text-gray-900  font-medium hover:text-red-600 transition-all ">Cart</span>
+                        <span className="text-gray-900 font-medium hover:text-red-600 transition-all">Cart</span>
                     </h1>
                 </section>
             </nav>
