72c844e2c1df6ae9f5d43c4e9afdbc6c4843f145
Vishal <vishal.bhumca21@gmail.com>
2024-09-04 09:59:52 +0530

    fixed cards items size

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -19,12 +19,12 @@ const ProductCard = ({ onAddToCart }) => {
     return (
         <>
             {products.map((product) => (
-                <section key={product.id} className="w-[150px] mb-4 min-w-[100px] min-h-[220px] flex rounded-md border-b-2 border-r-2 border-l border-t border-gray-600 shadow-lg transition-transform transform hover:scale-105 cursor-pointer hover:shadow-xl">
+                <section key={product.id} className="lg:w-[185px] sm:h-[280px] md:w-[170px] md:h-[250px] sm:w-[160px] mb-4 min-w-[100px] lg:h-[260px] min-h-[240px] flex rounded-md border-b-2 border-r-2 border-l border-t border-gray-600 shadow-lg transition-transform transform hover:scale-105 cursor-pointer hover:shadow-xl">
                     <section className="w-full h-full rounded flex items-center flex-col p-1">
                         <section className="bg-stone-700 rounded-md w-full h-[70%] hover:bg-stone-800"></section>
                         <section className="w-full h-[30%] flex flex-col items-center justify-center">
                             <p className="flex items-center justify-center font-semibold text-[0.6em] gap-x-1 pb-1">
-                                <span>{product.name.slice(0, 18)}</span>
+                                <span>{product.name.slice(0, 25)}</span>
                                 <span className="flex items-center font-bold text-gray-600">
                                     <span>₹</span>
                                     <span>{new Intl.NumberFormat('en-IN',{maximumSignificantDigits:3}).format(product.price)}</span>
